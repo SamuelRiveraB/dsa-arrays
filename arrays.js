f0d0a843b433f105605fc45acbb3c2f603723c4a
Samuel <samuelrivba@gmail.com>
2024-03-14 21:02:56 -0500

    Reverse a string

--- arrays.js
+++ arrays.js
@@ -1,6 +1,8 @@
 const strings = ["a", "b", "c", "d"];
 // 4*4 = 16 bytes of storage
 
+strings[2];
+
 // push
 strings.push("e"); // O(1)
 
@@ -11,6 +13,12 @@ strings.pop(); // O(1)
 strings.unshift("x"); // O(n)
 
 // splice
-strings.splice(2, 0, "alien");
+strings.splice(2, 0, "alien"); // O(n)
 
 console.log(strings);
+
+// C++
+// int a[20]
+// int b[5] {1,2,3,4,5}
+
+// Reverse a string means you have to convert it to an array
