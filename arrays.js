378ac5b824bc8ea7ccb7ccee9b9e1e40e23edf48
Samuel <samuelrivba@gmail.com>
2024-03-19 18:58:21 -0500

    Add pop function

--- arrays.js
+++ arrays.js
@@ -30,6 +30,19 @@ class MyArray {
   get(index) {
     return this.data[index];
   }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
 }
 
 // Reverse a string means you have to convert it to an array
