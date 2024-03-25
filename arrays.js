a988d924c7a68985b8d0700c9aa5f08dc1e47d22
Samuel <samuelrivba@gmail.com>
2024-03-25 18:26:30 -0500

    First solution attempt

--- arrays.js
+++ arrays.js
@@ -98,6 +98,10 @@ console.log(reverse3("Hi Im Samuel"));
 
 function mergeSortedArrays(arr1, arr2) {
   const mergedArray = [];
+  let arr1item = arr1[0];
+  let arr2item = arr2[0];
+  let i = 1;
+  let j = 1;
 
   // Check input
   if (arr1.length === 0) {
@@ -105,6 +109,18 @@ function mergeSortedArrays(arr1, arr2) {
   }
   if (arr2.length === 0) {
     return arr1;
+  }
+
+  while (arr1item || arr2item) {
+    if (arr1item < arr2item) {
+      mergedArray.push(arr1item);
+      arr1item = arr1[i];
+      i++;
+    } else {
+      mergedArray.push(arr2item);
+      arr2item = arr2[j];
+      j++;
+    }
   }
 }
 
